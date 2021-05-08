a64a362e85719a6bd3feaa42f67c3919a2735346
niklas pelle michelsen <niklaspelle@hotmail.com>
2021-05-08 13:51:49 +0200

    still not working

--- Assets/Scripts/Obstacles/ObstacleTrigger.cs
+++ Assets/Scripts/Obstacles/ObstacleTrigger.cs
@@ -6,17 +6,37 @@ public class ObstacleTrigger : MonoBehaviour
 {
     [SerializeField]
     private GameObject obstacle;
+    [SerializeField]
+    private float delay = 1f;
+    private bool isMoving;
+
+
+    void Start()
+    {
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (isMoving)
+        {
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        StartCoroutine(ExecuteAfterTime(delay));
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        isMoving = true;
+    }
+
+    public void ResetObstacle()
+    {
         
     }
-
-    void onTriggerEnter2D(Collider2D col)
-    {
-        obstacle.transform.Translate(0, 4, 0);
-        Debug.Log("GameObject1 collided with " + col.name);
-    }
-
 }
