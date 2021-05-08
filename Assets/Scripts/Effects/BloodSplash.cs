d96e829360a3ddf088faa54944c7387b2376d1f9
KacperKenjiLesniak <kenjikacper@gmail.com>
2021-05-08 17:06:50 +0200

    Dying in a blood splatter and restarting the level

---
+++ Assets/Scripts/Effects/BloodSplash.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace.Effects
+{
+    public class BloodSplash : MonoBehaviour
+    {
+        private void Start()
+        {
+            Invoke(nameof(DestroyBlood), 2f);
+        }
+
+        void DestroyBlood()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/PlayerDeath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MutableObjects.Int;
+using UnityEngine;
+
+namespace DefaultNamespace.Movement
+{
+    public class PlayerDeath : MonoBehaviour
+    {
+        [SerializeField] private MutableInt playersAlive;
+        [SerializeField] private GameObject bloodSplash;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Killer"))
+            {
+                playersAlive.Value -= 1;
+                Instantiate(bloodSplash, transform.position, Quaternion.identity);
+                GetComponent<Collider2D>().enabled = false;
+                GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
+    }
+}
