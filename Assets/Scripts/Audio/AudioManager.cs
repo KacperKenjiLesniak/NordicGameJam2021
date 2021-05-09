8006332bfab1f7e564726100f502c689267c5f24
KacperKenjiLesniak <kenjikacper@gmail.com>
2021-05-09 12:17:18 +0200

    Sounds and player characters

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace.Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        
+        private AudioSource[] sources;
+
+        private void Start()
+        {
+            sources = GetComponentsInChildren<AudioSource>();
+        }
+
+        public void PlayClip(int clip)
+        {
+            sources[clip].Play();
+        }
+        
+        public void StopAll()
+        {
+            foreach (var audioSource in sources)
+            {
+                audioSource.Stop();
+            }
+        }
+    }
+}
